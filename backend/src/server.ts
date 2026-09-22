824a9295f0d934fb8851c41e3e734bc38f2f7a68
mdrafi2038-glitch <mdrafi2038@gmail.com>
2026-09-22 20:02:56 +0600

    Use Railway injected PORT

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import { app } from './app.js'
 import { env } from './config/env.js'
 
-app.listen(env.APP_PORT, () => {
-  console.log(`Backend running on http://localhost:${env.APP_PORT}`)
+app.listen(env.APP_PORT, '0.0.0.0', () => {
+  console.log(`Backend running on port ${env.APP_PORT}`)
 })
